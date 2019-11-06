5d66f7a24089b689c09a4356939188f72a561da3
Tam <Tdna84@gmail.com>
2019-11-06 17:25:05 +0100

    first upload

---
+++ Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 2;
+            Console.WriteLine(num.GetType());
+        }
+    }
+}
