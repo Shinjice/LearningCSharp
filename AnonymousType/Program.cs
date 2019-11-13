06c487a55210114e65726b233a3a756d103090e3
Shinjice <Tdna84@gmail.com>
2019-11-13 16:54:49 +0100

    AnonymousType

---
+++ AnonymousType/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+                var myAnonymousType = new
+                {
+                    firstProperty = "First",
+                    secondProperty = 2,
+                    thirdProperty = true,
+                    anotherAnonymousType = new {nestedProperty = "Nested"}
+                };
+
+            Console.WriteLine(myAnonymousType.GetType().ToString());
+            Console.WriteLine(myAnonymousType);
+            Console.WriteLine(myAnonymousType.firstProperty);
+            Console.WriteLine(myAnonymousType.anotherAnonymousType.GetType().ToString());
+            Console.WriteLine(myAnonymousType.anotherAnonymousType.nestedProperty.ToString());
+            Console.WriteLine(myAnonymousType.anotherAnonymousType.nestedProperty);
+
+            DoSomethig(myAnonymousType);
+
+        }
+        static void DoSomethig(dynamic param)
+        {
+            Console.WriteLine(param.secondProperty);
+        }
+    }
+}
